233461a5e354dca38a214b5425b90812d7c8a362
Mohamad Khayat <moek91@outlook.com>
2018-03-21 22:07:43 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,11 +1,12 @@
-def fizzbuzz (int)
-  if int % 5 ==0 && int % 3 == 0
-    "FizzBuzz"
-
-  elsif int % 3 ==0
-    "Fizz"
-
+def fizzbuzz(int)
+  if int % 3 == 0 && int % 5 == 0
+    return "FizzBuzz"
+  elsif int % 3 == 0
+    return "Fizz"
   elsif int % 5 == 0
-    "Buzz"
+    return "Buzz"
+  else
+    return nil
   end
 end
+
